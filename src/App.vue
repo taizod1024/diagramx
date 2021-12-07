f8e3e43c5cc5753e9cc9d876931c60455529e087
yamamoto.taizo <taizod1024@gmail.com>
2021-12-07 14:43:23 +0900

    fix: typescriptデバッグ確認 #6、疎通完了

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import BootstrapTest from "./components/BootstrapTest.vue";
 import { defineComponent, onMounted, ref } from "vue";
 export default defineComponent({
