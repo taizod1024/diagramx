c7b2cb03f4ee2462453585c52f78a708ed7cc066
yamamoto.taizo <taizod1024@gmail.com>
2021-12-07 13:39:18 +0900

    fix: #22 vue3対応、bootstrap5動作、API動作不可

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
- <div>
-   <BootstrapTest />
- </div>
+  <div>
+    {{ message }}
+    <hr />
+    <BootstrapTest />
+  </div>
 </template>
 
 <script>
-import BootstrapTest from './components/BootstrapTest.vue'
- 
-export default {
- name: 'App',
- components: {
-   BootstrapTest
- }
-}
+import BootstrapTest from "./components/BootstrapTest.vue";
+import { defineComponent, onMounted, ref } from "vue";
+export default defineComponent({
+  components: {
+    BootstrapTest,
+  },
+  setup() {
+    let message = ref("(undefined)");
+    onMounted(() => {
+      message = ref("abc");
+    });
+    return {
+      message
+    };
+  },
+});
 </script>
 
 <style>
 #app {
- font-family: Avenir, Helvetica, Arial, sans-serif;
- -webkit-font-smoothing: antialiased;
- -moz-osx-font-smoothing: grayscale;
- text-align: center;
- color: #2c3e50;
- margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
