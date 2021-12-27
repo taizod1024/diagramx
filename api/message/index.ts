63dd1f3ae74ffc16499379324f3d78fd8367b3fc
yamamoto.taizo <taizod1024@gmail.com>
2021-12-27 18:03:24 +0900

    refactor: #9 本番環境でのhttp500切り分け@12

--- api/message/index.ts
+++ api/message/index.ts
@@ -3,37 +3,35 @@ import { UserInfo } from '../common/UserInfo';
 import azurestorage from 'azure-storage';
 
 const httpTrigger: AzureFunction = async function (context) {
-  try {
-    let info: any = {};
-    // table storage
-    let tableservice = azurestorage.createTableService();
-    tableservice.createTableIfNotExists(
-      'diagramt',
-      function (error, result, response) {
-        info.tableerror = error;
-      }
-    );
-    // blob storage
-    let blobservice = azurestorage.createBlobService();
-    blobservice.createContainerIfNotExists(
-      'diagramb',
-      function (error, result, response) {
-        info.bloberror = error;
-      }
-    );
-    // response
-    const userinfo = new UserInfo();
-    userinfo.get(context);
-    info.userdisplayname = userinfo.displayName;
-    context.res = {
-      headers: { 'Content-Type': 'application/json' },
-      body: {
-        text: `swa api(${JSON.stringify(info)})`,
-      },
-    };
-  } catch (err) {
-    console.log(err);
-  }
+  console.log('aaa_begin');
+  let info: any = {};
+  // table storage
+  let tableservice = azurestorage.createTableService();
+  tableservice.createTableIfNotExists(
+    'diagramt',
+    function (error, result, response) {
+      info.tableerror = error;
+    }
+  );
+  // blob storage
+  let blobservice = azurestorage.createBlobService();
+  blobservice.createContainerIfNotExists(
+    'diagramb',
+    function (error, result, response) {
+      info.bloberror = error;
+    }
+  );
+  // response
+  const userinfo = new UserInfo();
+  userinfo.get(context);
+  info.userdisplayname = userinfo.displayName;
+  context.res = {
+    headers: { 'Content-Type': 'application/json' },
+    body: {
+      text: `swa api(${JSON.stringify(info)})`,
+    },
+  };
+  console.log('aaa_end');
 };
 
 export default httpTrigger;
