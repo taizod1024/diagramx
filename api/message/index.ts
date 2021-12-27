a159a2af8e1034a05f7c174a5ec22e5149e259aa
yamamoto.taizo <taizod1024@gmail.com>
2021-12-27 16:13:59 +0900

    refactor: #9 本番環境でのhttp500切り分け@8

--- api/message/index.ts
+++ api/message/index.ts
@@ -1,15 +1,34 @@
 import { AzureFunction } from '@azure/functions';
 import { UserInfo } from '../common/UserInfo';
-// import azurestorage from 'azure-storage';
+import azurestorage from 'azure-storage';
 
 const httpTrigger: AzureFunction = async function (context) {
   try {
+    let info: any = {};
+    // table storage
+    let tableservice = azurestorage.createTableService();
+    tableservice.createTableIfNotExists(
+      'diagramt',
+      function (error, result, response) {
+        info.tableerror = error;
+      }
+    );
+    // blob storage
+    let blobservice = azurestorage.createBlobService();
+    blobservice.createContainerIfNotExists(
+      'diagramb',
+      function (error, result, response) {
+        info.bloberror = error;
+      }
+    );
+    // response
     const userinfo = new UserInfo();
     userinfo.get(context);
+    info.userdisplayname = userinfo.displayName;
     context.res = {
       headers: { 'Content-Type': 'application/json' },
       body: {
-        text: `Hello from the API(${userinfo.displayName})`,
+        text: `swa api(${JSON.stringify(info)})`,
       },
     };
   } catch (err) {
