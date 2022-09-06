18c5e344a09cff06c74db385748e61299cdeaa23
yamamoto.taizo <taizod1024@gmail.com>
2022-09-06 16:52:17 +0900

    refactor: コード整理

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,36 +11,54 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'AllDiagrams',
     component: AllDiagrams,
+    meta: {
+      title: 'all diagrams',
+    },
   },
   {
     // user diagrams
     path: '/user',
     name: 'UserDiagram',
     component: UserDiagrams,
+    meta: {
+      title: 'user diagrams',
+    },
   },
   {
     // sign up
     path: '/signup',
     name: 'SignUp',
     component: SignUp,
+    meta: {
+      title: 'sign up',
+    },
   },
   {
     // sign in
     path: '/signin',
     name: 'SignIn',
     component: SignIn,
+    meta: {
+      title: 'sign in',
+    },
   },
   {
     // your diagrams
     path: '/my',
     name: 'MyDiagram',
     component: () => import('../views/MyDiagrams.vue'),
+    meta: {
+      title: 'my diagrams',
+    },
   },
   {
     // new diagrams
     path: '/new',
     name: 'NewDiagram',
     component: () => import('../views/NewDiagram.vue'),
+    meta: {
+      title: 'new diagram',
+    },
   },
 ];
 
@@ -49,5 +67,13 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
+const DEFAULT_TITLE = 'diagramx';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+router.afterEach((to, _from) => {
+  let title = DEFAULT_TITLE;
+  if (to.meta.title) {
+    title += ' - ' + to.meta.title;
+  }
+  document.title = title;
+});
 export default router;
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,13 +2,19 @@
   <div class="signin mt-4">
     <h5>sign in with</h5>
     <div class="container col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 col-xxl-2">
-      <a class="col-12 m-1 mr-1 btn btn-outline-dark" href="/login/github"
+      <a
+        class="col-12 m-1 mr-1 btn btn-sm btn-outline-dark"
+        href="/login/github"
         >github</a
       >
-      <a class="col-12 m-1 mr-1 btn btn-outline-dark" href="/login/twitter"
+      <a
+        class="col-12 m-1 mr-1 btn btn-sm btn-outline-dark"
+        href="/login/twitter"
         >twitter</a
       >
-      <a class="col-12 m-1 mr-1 btn btn-outline-dark" href="/login/google"
+      <a
+        class="col-12 m-1 mr-1 btn btn-sm btn-outline-dark"
+        href="/login/google"
         >google</a
       >
     </div>
